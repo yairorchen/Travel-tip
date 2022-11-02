e1accaabacbef55b40d4519d27dded9257cc7055
Yair Orchen <yairorchen1@gmail.com>
2022-11-02 16:31:36 +0200

    make fundamantal copy for continuing devopment

--- js/app.controller.js
+++ js/app.controller.js
@@ -8,46 +8,75 @@ window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
 
 function onInit() {
-    mapService.initMap()
-        .then(() => {
-            console.log('Map is ready')
-        })
-        .catch(() => console.log('Error: cannot init map'))
+  mapService
+    .initMap()
+    .then(() => {
+      console.log('Map is ready')
+    })
+    .catch(() => console.log('Error: cannot init map'))
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos')
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
+  console.log('Getting Pos')
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
 }
 
 function onAddMarker() {
-    console.log('Adding a marker')
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
+  console.log('Adding a marker')
+  mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
 }
 
 function onGetLocs() {
-    locService.getLocs()
-        .then(locs => {
-            console.log('Locations:', locs)
-            document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
-        })
+  locService.getLocs().then((locs) => {
+    console.log('Locations:', locs)
+    document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
+  })
 }
 
 function onGetUserPos() {
-    getPosition()
-        .then(pos => {
-            console.log('User position is:', pos.coords)
-            document.querySelector('.user-pos').innerText =
-                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-        })
-        .catch(err => {
-            console.log('err!!!', err)
-        })
+  getPosition()
+    .then((pos) => {
+      console.log('User position is:', pos.coords)
+      document.querySelector(
+        '.user-pos'
+      ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
+    })
+    .catch((err) => {
+      console.log('err!!!', err)
+    })
 }
+
 function onPanTo() {
-    console.log('Panning the Map')
-    mapService.panTo(35.6895, 139.6917)
+  console.log('Panning the Map')
+  mapService.panTo(35.6895, 139.6917)
+}
+
+//////
+
+function renderPlaces() {
+  const places = getPlaces()
+  const elList = document.querySelector('.place-list')
+  const strHtmls = places
+    .map(({ id, name }) => {
+      return `
+        <li class="flex clean-list">
+        <h1>${name}</h1>
+        <button onclick="onPanToPlace('${id}')">GO</button>
+        <button onclick="onRemovePlace('${id}')">X</button>
+      </li>
+        `
+    })
+    .join('')
+  // console.log('strHtmls', strHtmls);
+  elList.innerHTML = strHtmls
+}
+
+function onRemovePlace(placeId) {
+  console.log('placeId', placeId)
+  removePlace(placeId)
+  renderPlaces()
+  renderMarkers()
 }
--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,71 @@
 export const locService = {
-    getLocs
+  getLocs,
 }
 
+function _createPlace(
+  name = 'puki land',
+  lat,
+  lng,
+  zoom,
+  createdAt = Date.now(),
+  updatedAt = null
+) {
+  return { id: makeId(), lat, lng, name, zoom, createdAt, updatedAt }
+}
 
+function removePlace(placeId) {
+  gPlaces = gPlaces.filter((p) => p.id !== placeId)
+  _savePlacesToStorage()
+}
+
+function addPlace(name, lat, lng, zoom) {
+  //{ id: makeId(), lat, lng, name, zoom }
+  gPlaces.unshift({ id: makeId(), lat, lng, name, zoom })
+  _savePlacesToStorage()
+}
+function getPlaceById(placeId) {
+  return gPlaces.find((p) => p.id === placeId)
+}
+function _createPlaces() {
+  const places = loadFromStorage(STORAGE_KEY_PLACES) || []
+  if (!places || !places.length) {
+    for (let i = 0; i < 3; i++) {
+      const placeName = 'DemoPlace' + (i + 1)
+      places.push(_createPlace(placeName, 33 + i, 35 + i, 10))
+    }
+  }
+  gPlaces = places
+  _savePlacesToStorage()
+}
+
+function _savePlacesToStorage() {
+  saveToStorage(STORAGE_KEY_PLACES, gPlaces)
+}
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+  {
+    id: 1,
+    name: 'Greatplace',
+    lat: 32.047104,
+    lng: 34.832384,
+    weather: null,
+    createdAt: null,
+    updatedAt: null,
+  },
+  {
+    id: 2,
+    name: 'Neveragain',
+    lat: 32.047201,
+    lng: 34.832581,
+    weather: null,
+    createdAt: null,
+    updatedAt: null,
+  },
 ]
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs)
-        }, 2000)
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs)
+    }, 2000)
+  })
 }
-
-
